4d84e5861779039505752e2d5c035e66541f780c
Ferran Puig Calvache <ferran.jr@gmail.com>
2016-02-24 23:10:09 +0000

    STEP 1 - parsing strings to create proper Items

--- src/main/scala/com/ferran/Checkout.scala
+++ src/main/scala/com/ferran/Checkout.scala
@@ -1,8 +1,40 @@
 package com.ferran
 
 
+// Items can be added to a basket
+sealed abstract class Item( val price: BigDecimal )
+case object Apple extends Item(0.6)
+case object Orange extends Item(0.25)
+
+object Item {
+
+  /**
+   * Parse Input String to Item, if valid string
+   *
+   * @param in string to be parsed
+   * @return
+   */
+  def fromString(in: String): Option[Item] =
+    in match {
+      case "apple" => Some(Apple)
+      case "orange" => Some(Orange)
+      case _ => None
+    }
+}
 
 object Checkout {
+
+  // Basket representation
+  type Basket = Seq[Item]
+
+  object Basket{
+    def apply(arr: Array[String]): Seq[Item] = {
+      arr.map(Item.fromString)
+        .filter(_.isDefined)
+        .map(_.get)
+    }
+  }
+
 
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
--- src/test/scala/CheckoutSpecs.scala
+++ src/test/scala/CheckoutSpecs.scala
@@ -1,6 +1,37 @@
+import com.ferran.Checkout.Basket
+import com.ferran.{Orange, Apple, Item}
 import org.specs2.mutable.Specification
 
 class CheckoutSpecs extends Specification {
 
-  ???
+  """Item""" should {
+
+    """Create item from a single valid String""" in {
+      val stringApple = "apple"
+      val stringOrange = "orange"
+      val stringUnknown = "unkown"
+
+      Item.fromString(stringApple) === Some(Apple)
+      Item.fromString(stringOrange) === Some(Orange)
+      Item.fromString(stringUnknown) === None
+    }
+  }
+
+  """Basket""" should {
+
+    """Create a proper basket from and empty array""" in {
+      val test: Array[String] = Array.empty[String]
+      Basket(test) === Seq.empty[Item]
+    }
+
+    """Create a proper basket from all valid strings""" in {
+      val test: Array[String] = Array("apple", "orange", "apple")
+      Basket(test) === Seq(Apple, Orange, Apple)
+    }
+
+    """Create a proper basket with only the valid strings, ignore rest""" in {
+      val test: Array[String] = Array("apple", "orange", "apple", "unkown")
+      Basket(test) === Seq(Apple, Orange, Apple)
+    }
+  }
 }
